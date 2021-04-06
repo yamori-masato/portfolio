a94b18eb84a0d42bc5f4ae814519014060fc345a
yamori-masato <yamorimasato@gmail.com>
2021-04-07 02:27:28 +0900

    add p5 sketch

---
+++ src/sketches/background.ts
@@ -0,0 +1,45 @@
+import p5 from 'p5'
+import ZodiacSign from './components/ZodiacSign'
+
+export type SketchProps = {
+  onComplete: () => void
+}
+
+const sketch = (p: p5) => {
+  let count: number
+  let delay: number
+  let flag: boolean
+  let z: ZodiacSign
+
+  p.setup = () => {
+    p.createCanvas(p.windowWidth, p.windowHeight)
+    p.frameRate(30)
+    delay = 30
+    initializeBackground()
+  }
+  p.draw = () => {
+    count++
+    p.background(0)
+    if (count < delay) return
+    if (count === delay+30) z.toggleMove()
+
+    z.update()
+    z.display()
+  }
+  p.mouseClicked = () => {
+    z.flash()
+  }
+
+  p.windowResized = () => {
+    p.resizeCanvas(p.windowWidth, p.windowHeight)
+    initializeBackground()
+  }
+
+  function initializeBackground() {
+    z = new ZodiacSign(p, 0, 0, p.width, p.height)
+    flag = false
+    count = 0
+  }
+}
+
+export default sketch
