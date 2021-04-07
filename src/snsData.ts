7d26be1014214ddbd284d7f9fbde8c1d0a6f9943
yamori-masato <yamorimasato@gmail.com>
2021-04-07 22:37:14 +0900

    add snsData

---
+++ src/snsData.ts
@@ -0,0 +1,49 @@
+export const snsData = [
+  {
+    name: "github",
+    link: "https://github.com/yamori-masato",
+    svg: {
+      url: "./images/github.svg",
+      style: {
+        fill: "#fff",
+        background: "#181717"
+      }
+    },
+  },
+  {
+    name: "zenn",
+    link: "https://zenn.dev/yamo",
+    svg: {
+      url: "./images/zenn.svg",
+      style: {
+        fill: "#3EA8FF",
+        background: "#323030",
+        transform: "scale(0.75)"
+      }
+    },
+  },
+  {
+    name: "qiita",
+    link: "https://qiita.com/yamooo",
+    svg: {
+      url: "./images/qiita.svg",
+      style: {
+        fill: "#fff",
+        background: "#55c500",
+        transform: "scale(0.85)"
+      }
+    },
+  },
+  {
+    name: "twitter",
+    link: "",
+    svg: {
+      url: "./images/twitter.svg",
+      style: {
+        fill: "#fff",
+        background: "#1DA1F2",
+        transform: "scale(0.65)"
+      }
+    },
+  },
+]
