306cba4fa8e4fe1f118ce54fa09d0bc36ecda60a
yamori-masato <yamorimasato@gmail.com>
2021-04-07 22:37:01 +0900

    add SnsIconList component

---
+++ src/components/SnsIconList.tsx
@@ -0,0 +1,45 @@
+import React, { FC } from 'react'
+import styled from 'styled-components'
+import SnsIcon, { TSns } from './SnsIcon'
+
+const StyledContainer = styled.div`
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+
+    li {
+      flex: 1;
+      padding-left: 2rem;
+
+      &:first-child {
+        padding-left: 0;
+      }
+    }
+  }
+`
+
+type Props = {
+  data: TSns[]
+}
+
+const SnsIconList: FC<Props> = ({ data }) => {
+  return (
+    <StyledContainer>
+      <ul>
+        {data.map((v, i) => (
+          <li key={i.toString()}>
+            <SnsIcon
+              name={v.name}
+              link={v.link}
+              svg={v.svg}
+            />
+          </li>
+        ))}
+      </ul>
+    </StyledContainer>
+  )
+}
+
+export default SnsIconList
