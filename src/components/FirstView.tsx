8d102b6e782a35a5bbc0a4e4be5caad093aef10a
yamori-masato <yamorimasato@gmail.com>
2021-04-12 19:07:31 +0900

    fix

--- src/components/FirstView.tsx
+++ src/components/FirstView.tsx
@@ -7,32 +7,35 @@ const StyledSection = styled.section`
   position: relative;
   height: 100vh;
   width: 100%;
+  display: flex;
+  align-items: center;
   user-select: none;
   margin-bottom: 10vh;
 
   h1 {
-    position: absolute;
-    top: 50%;
-    left: 50%;
     width: 100%;
     text-align: center;
-    transform: translate(-50%, -50%);
     color: #fff;
     font-size: 3rem;
     letter-spacing: 40px;
+    text-indent: 40px;
     user-select: none;
     ${tab`
       letter-spacing: 3.3vw;
+      text-indent: 3.3vw;
       font-size: 2.8rem;
     `}
     ${sp`
       letter-spacing: 3.3vw;
+      text-indent: 3.3vw;
       font-size: 1.8rem;
     `}
   }
 
   a {
     position: absolute;
+    font-size: 3rem;
+    color: white;
     bottom: 5%;
     left: 50%;
     transform: translateX(-50%);
