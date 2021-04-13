3f59bb98d0e3391cf1799bcf8faa9bba1faefb47
yamori-masato <yamorimasato@gmail.com>
2021-04-13 15:20:57 +0900

    add Works page component and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom'
+import {
   Top,
   Works,
 } from 'pages'
+import Layout from 'components/Layout'
 
 const App = () => {
   return (
-    <>
-      <Top />
-    </>
+    <Router>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Top} />
+          <Route exact path="/works" component={Works} />
+        </Switch>
+      </Layout>
+    </Router>
   )
 }
 
---
+++ src/pages/Works.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Works = () => {
+  return (
+    <div>
+      This is Works page!!
+    </div>
+  )
+}
+
+export default Works
