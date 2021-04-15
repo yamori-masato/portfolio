667d035ee47946ff03dc6e72353b00a151b1276f
yamori-masato <yamorimasato@gmail.com>
2021-04-16 03:55:02 +0900

    add Thumbnail component

---
+++ src/components/Thumbnail.tsx
@@ -0,0 +1,68 @@
+import React, { FC } from 'react'
+import styled from 'styled-components'
+import YouTube from 'react-youtube'
+
+// TODO: Cardと共通化
+const StyledBase = styled.div`
+  width: 100%;
+  height: 100%;
+  .wrapper {
+    background-color: rgba(15, 15, 15, 0.7);
+    position: relative;
+    width: 100%;
+    margin: 0;
+    padding-bottom: 56.25%;
+    overflow: hidden;
+    border-radius: 8px;
+
+    & > * {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+`
+
+type Props = {
+  image: string
+  video?: string
+}
+
+const Thumbnail: FC<Props> = (props) => {
+  console.log(props.video)
+  return (
+    <StyledBase>
+      {
+        props.video ? (
+          <VideoThumbnail {...props} />
+        ) : (
+          <ImageThumbnail {...props} />
+        )
+      }
+    </StyledBase>
+  )
+}
+
+// ----------
+
+const ImageThumbnail: FC<Props> = (props) => {
+  return (
+    <div className="wrapper">
+      <img src={props.image}/>
+    </div>
+  )
+}
+
+// ----------
+
+const VideoThumbnail: FC<Props> = (props) => {
+  return (
+    <YouTube
+      videoId={props.video}
+      containerClassName="wrapper"
+    />
+  )
+}
+
+export default Thumbnail
--- src/pages/WorkDetail.tsx
+++ src/pages/WorkDetail.tsx
@@ -1,16 +1,43 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
+import PageWrapper from './PageWrapper'
+import styled from 'styled-components'
+import { pc, sp, tab } from 'media'
+import { headerHeight } from 'constants/styles'
+import works from 'data/works'
 import { RouteComponentProps } from 'react-router-dom'
+import Thumbnail from 'components/Thumbnail'
 
-type Props = RouteComponentProps<{id: string}> & {
+// TODO: Workと共通化
+const StyledContainer = styled.div`
+  min-height: 100%;
+  width: 70%;
+  max-width: 1000px;
+  margin: 0 auto;
+  padding-top: calc(${headerHeight}px + 20px);
+  ${sp`
+    padding-top: calc(${headerHeight}px + 20px);
+  `}
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`
 
-} 
+type Props = RouteComponentProps<{id: string}> & { } 
 
 const WorkDetail: FC<Props> = (props) => {
-  return (
-    <div>
-      {props.match.params.id}
-    </div>
-  )
+  const [work, setWork] = useState<schema.Work | undefined>(undefined)
+  useEffect(() => {
+    const selected = works.find(w => (w.id.toString() === props.match.params.id))
+    setWork(selected)
+  }, [props.match.params.id])
+
+  return work ? (
+    <PageWrapper>
+      <StyledContainer>
+        <Thumbnail image={work.thumbnail} video={work?.video}/>
+      </StyledContainer>
+    </PageWrapper>
+  ) : (<></>)
 }
 
 export default WorkDetail
