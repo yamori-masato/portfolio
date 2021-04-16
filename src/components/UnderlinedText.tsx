0a0abfee5d1ddbc77ef20fc14d8825f7b1096bc5
yamori-masato <yamorimasato@gmail.com>
2021-04-17 00:20:51 +0900

    fix width

--- src/components/UnderlinedText.tsx
+++ src/components/UnderlinedText.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import { sp } from 'media'
 
 const StyledTitle = styled.div`
+  width: max-content;
   display: flex;
   justify-content: center;
   align-items: center;
