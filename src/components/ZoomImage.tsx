0bceaf2701e94f5b1b73a6ed6b2bb366a3e53d0a
yamori-masato <yamorimasato@gmail.com>
2021-04-17 16:13:07 +0900

    fix margin

--- src/components/ZoomImage.tsx
+++ src/components/ZoomImage.tsx
@@ -1,14 +1,20 @@
 import React, { FC } from 'react'
 import Zoom from 'react-medium-image-zoom'
 import 'react-medium-image-zoom/dist/styles.css'
+import useMediaQuery from 'hooks/useMediaQuery'
 import Image from './Image'
 
 type Props = React.ImgHTMLAttributes<HTMLImageElement>
 
 const ZoomImage: FC<Props> = (props) => {
+  const mq = useMediaQuery()
+  let margin = 10
+  if (mq.isTab) margin = 50
+  if (mq.isPc) margin = 120
+
   return (
     <Zoom
-      zoomMargin={120}
+      zoomMargin={margin}
       overlayBgColorStart={'rgba(0, 0, 0, 0)'}
       overlayBgColorEnd={'rgba(0, 0, 0, 0.75)'}
     >
