699bed39399cfbc20786d45e4046445c92cc6afe
yamori-masato <yamorimasato@gmail.com>
2021-04-19 02:05:53 +0900

    use Icon component

--- src/components/SnsIcon.tsx
+++ src/components/SnsIcon.tsx
@@ -1,25 +1,16 @@
 import React, { FC } from 'react'
 import styled from 'styled-components'
-import { ReactSVG } from 'react-svg'
+import Icon from './Icon'
 
 const StyledLink = styled.a<{background?: string | number}>`
   display: block;
-  width: 3.4rem;
-  height: 3.4rem;
   border-radius: 50%;
-  overflow: hidden;
-  background: ${({ background }) => background ? background : 'none'};
   border: solid 2px #434343;
   transition: .5s;
   opacity: 0.9;
   
   &:hover {
     border: solid 2px #7d7d7d;
-  }
-
-  & > div {
-    width: 100%;
-    height: 100%;
   }
 `
 
@@ -41,9 +32,11 @@ const SnsIcon: FC<TSns> = ({ name, link, svg }) => {
       rel="noopener noreferrer"
       className="cursor"
     >
-      <ReactSVG
-        src={svg.url}
+      <Icon
+        svg={svg.url}
         style={svg.style}
+        size={34}
+        background={svg.style?.background}
       />
     </StyledLink>
   )
