e6545a0363accf14402c81c19b678da74718cea2
yamori-masato <yamorimasato@gmail.com>
2021-04-19 01:37:41 +0900

    add ViewSourceLink component

---
+++ src/components/ViewSourceLink.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components'
+import { snsData } from 'data/sns'
+import Icon from 'components/Icon'
+import Link from './Link'
+
+// TODO: snsDataに依存しないようにしたい
+const ViewSourceLink = () => {
+  const github = snsData.find(v => v.name === "github")
+  return github ? (
+    <Link
+      href={github.link}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="cursor"
+    >
+      <Icon
+        svg={github.svg.url}
+        style={github.svg.style}
+        size={16}
+      />
+      <div>View source</div>
+    </Link>
+  ) : (<></>)
+}
+
+export default ViewSourceLink
