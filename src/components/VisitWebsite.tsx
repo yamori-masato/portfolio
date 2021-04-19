5abc8e31820f1aa8baf2d495f240074b1cb40ecf
yamori-masato <yamorimasato@gmail.com>
2021-04-19 11:40:28 +0900

    add VisitWebsite component

---
+++ src/components/VisitWebsite.tsx
@@ -0,0 +1,30 @@
+import React, { FC } from 'react'
+import { snsData } from 'data/sns'
+import Icon from 'components/Icon'
+import { ReactSVG } from 'react-svg'
+import Link from './Link'
+import icon from 'assets/images/link.svg'
+
+type Props = {
+  url: string
+}
+
+// TODO: snsDataに依存しないようにしたい
+const VisitWebsite: FC<Props> = ({ url }) => {
+  return (
+    <Link
+      href={url}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="cursor"
+    >
+      <ReactSVG
+        src={icon}
+        style={{display: 'flex', alignItems: 'center', height: '10px'}}
+      />
+      <div>Visit website</div>
+    </Link>
+  )
+}
+
+export default VisitWebsite
