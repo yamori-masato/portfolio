81ff99ae6787d6449a62b5d53f8c9671bbe28e91
yamori-masato <yamorimasato@gmail.com>
2021-04-19 23:09:30 +0900

    update WorkAbout component

--- src/components/WorkAbout.tsx
+++ src/components/WorkAbout.tsx
@@ -1,41 +1,62 @@
 import React, { FC } from 'react'
 import styled from 'styled-components'
 import { headerHeight } from 'constants/styles'
+import ViewSourceLink from './ViewSourceLink'
+import VisitWebsite from './VisitWebsite'
+import SpeechBalloon from './SpeechBalloon'
 
 const StyledContainer = styled.div`
+  position: sticky;
+  top: ${headerHeight}px;
+`
+
+const StyledOverview = styled.div`
   width: 100%;
   padding: 20px;
   background-color: var(--ui-background);
   border-radius: 8px;
-  border: solid 1px rgba(255,255,255,0.5);
-
-  position: sticky;
-  top: ${headerHeight}px;
+  border: solid 1px var(--box-border-color);
+  color: #fff;
 
   ul {
-    list-style: none;
-    margin: 0;
-    padding: 0;
     display: flex;
     flex-direction: column;
     li {
-      margin-top: 24px;
-      color: #fff;
-      font-size: 1.8rem;
-      span {
-        font-size: 2.0rem;
-        font-weight: bold;
-        padding-right: 0.5em;
+      display: flex;
+      align-items: flex-end;
+      margin-bottom: 24px;
+      &:last-child {
+        margin-bottom: 0;
       }
 
-      &:first-child {
-        margin-top: 0;
-        text-align: center;
-        font-weight: bold;
-        font-size: 2.4rem;
-        font-weight: bold;
+      & > div {
+        margin-left: 0.5em;
+        font-size: 1.8rem;
+        &:first-child {
+          margin-left: 0;
+          font-size: 2.0rem;
+          font-weight: bold;
+        }
+        &:nth-child(3) {
+          padding-bottom: 1px;
+        }
       }
     }
+  }
+`
+const StyledTitle = styled.h2`
+  margin-top: 0;
+  text-align: center;
+  font-size: 2.4rem;
+  font-weight: bold;
+`
+
+const StyledLinks = styled.div`
+  margin-top: 20px;
+  ul {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
   }
 `
 
@@ -44,14 +65,49 @@ type Props = schema.Work
 const WorkAbout: FC<Props> = (props) => {
   return (
     <StyledContainer>
-      <ul>
-        <li>{props.title}</li>
-        <li><span>Genre:</span>{props.genre.join('/')}</li>
-        <li><span>Role:</span>{props.role.join('/')}</li>
-        <li><span>Skills:</span>{props.skills.join('/')}</li>
-        <li><span>Period:</span>{props.period}</li>
-      </ul>
+      <Overview {...props} />
+      <Links {...props} />
     </StyledContainer>
+  )
+}
+
+const Overview: FC<Props> = (props) => {
+  return (
+    <StyledOverview>
+      <StyledTitle>{props.title}</StyledTitle>
+      <ul>
+        <li>
+          <div>Genre:</div>
+          <div>{props.genre.join('/')}</div>
+        </li>
+        <li>
+          <div>Role:</div>
+          <div>{props.role.join('/')}</div>
+        </li>
+        <li>
+          <div>Skills:</div>
+          <div>{props.skills.join('/')}</div>
+          <SpeechBalloon keywords={props.keywords}/>
+        </li>
+        <li>
+          <div>Period:</div>
+          <div>{props.period}</div>
+        </li>
+      </ul>
+    </StyledOverview>
+  )
+}
+
+const Links: FC<Props> = ({ urls }) => {
+  const repo = urls?.repository
+  const site = urls?.site
+  return (
+    <StyledLinks>
+      <ul>
+        { site && <li><VisitWebsite url={site} /></li> }
+        { repo && <li><ViewSourceLink url={repo} /></li> }
+      </ul>
+    </StyledLinks>
   )
 }
 
