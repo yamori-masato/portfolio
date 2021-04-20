d4d3b23baa2ce53835acd92f682fead07261cdc5
yamori-masato <yamorimasato@gmail.com>
2021-04-20 13:09:22 +0900

    remove some dead code

--- src/ScrollToTop.ts
+++ src/ScrollToTop.ts
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import { useEffect } from "react"
 import { useLocation } from "react-router-dom"
 
 const ScrollToTop = () => {
@@ -6,7 +6,6 @@ const ScrollToTop = () => {
   useEffect(() => {
     const element = document.getElementById('scroll-container')
     if (element) {
-      const position = element?.scrollHeight
       element.scrollTop = 0
     }
   }, [pathname])
