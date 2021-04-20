cace05011b791737fc1ac0767e13f929d391d8e6
yamori-masato <yamorimasato@gmail.com>
2021-04-20 13:09:51 +0900

    remove some dead code

--- src/components/Burger.tsx
+++ src/components/Burger.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react'
+import React, { FC } from 'react'
 import styled, { css } from 'styled-components'
 
 const StyledContainer = styled.div<{isOpen: boolean}>`
