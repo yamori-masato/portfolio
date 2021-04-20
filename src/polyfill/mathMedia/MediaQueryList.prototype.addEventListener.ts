909eaf0c477e7251e96167bdb87e7f3ecc92b146
yamori-masato <yamorimasato@gmail.com>
2021-04-20 16:33:31 +0900

    fix isolatedModules flag

---
+++ src/polyfill/mathMedia/MediaQueryList.prototype.addEventListener.ts
@@ -0,0 +1,15 @@
+if (typeof matchMedia !== "undefined" && !matchMedia("all").addEventListener) {
+  console.log('installing polyfill: MediaQueryList.prototype.addEventListener')
+
+  const originalMatchMedia = matchMedia
+  window.self.matchMedia = function matchMedia(mediaQuery: string): MediaQueryList {
+    const mql = originalMatchMedia(mediaQuery)
+    mql.addEventListener = function (eventName: "change", listener: EventListenerOrEventListenerObject) {
+      // @ts-ignore
+      this.addListener(listener)
+    }
+    return mql
+  };
+}
+
+export {}
